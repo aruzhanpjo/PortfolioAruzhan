b18206234cddd2c46b7bb1290318f165abfaed50
aruzhanpjo <aruzhan.betigenova@mnsu.edu>
2023-11-26 00:35:53 -0600

    added more changes

--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -1,11 +1,23 @@
 import './index.scss';
 import Sidebar from '../Sidebar';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
-    <>
-    < Sidebar />
-    </>
+    <div className="App">
+        < Sidebar />
+        <div className='page'> 
+            <span className='tags top-tags'>&lt;body&gt;</span>
+
+            <Outlet />
+
+            <span className='tags bottom-tags'>
+                &lt;/body&gt;
+                <br/>
+                <span className='bottom-tag-html'></span>
+            </span>
+        </div>
+    </div>
     )
 }
 
--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import LogoA from '../../assets/images/logo-a.png';
 import LogoSubstitles from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -12,6 +15,38 @@ const Sidebar = () => (
             <img src={LogoA} alt="logo" />
             <img className="sub-logo" src={LogoSubstitles} alt="slobodan" />
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            className ="about-link" 
+            to="/about"
+            >
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
+
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/aruzhan-betigenova-a949501b1/">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://github.com/aruzhanpjo">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
