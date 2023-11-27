3a2e208d92f69a432ef1c52eef9ae1426d3e4253
aruzhanpjo <aruzhan.betigenova@mnsu.edu>
2023-11-27 11:58:36 -0600

    draft

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,1 +1,21 @@
-const Home = () => ('Hello')
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-a.png'
+import './index.scss'
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm</h1>
+                <img src={LogoTitle} alt="developer" />
+                ruzhan
+                <br />
+                web developer
+
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+
+        </div>
+    );
+}
+
+export default Home
