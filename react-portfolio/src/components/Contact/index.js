cbc78aae1bc63e8a80cbf7cfb2e0f6532926a2cf
aruzhanpjo <aruzhan.betigenova@mnsu.edu>
2023-12-27 18:32:31 +0600

    finished the form

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -26,6 +26,36 @@ const Contact = () => {
                     <p>
                         I am interested in networking! So, don't hesitate to contact me!
                     </p>
+                    <div className="contact-form">
+                        <form>
+                            <ul>
+                                <li className='half'>
+                                    <input placeholder='Name' type='text' name='name' /> 
+                                </li>    
+
+                                <li className='half'>
+                                    <input placeholder='Email' type='email' name='email' required />
+                                </li>
+
+                                <li>
+                                <input placeholder='Subject' type='text' name='subject' required />
+                                </li>
+
+                                <li>
+                                    <textarea placeholder='Message' name='message' required></textarea>
+                                </li>
+
+                                <li>
+                                    <input type='submit' className='flat-button' value='SEND' />
+                                </li>
+                                
+                            </ul>
+                        </form>
+
+                    </div>
+
+                    
+
 
                 </div>
 
