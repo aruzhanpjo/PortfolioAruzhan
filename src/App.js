6b7eea5b9e35e3bd26fbe038f4bd79c2c4398575
aruzhanpjo <aruzhan.betigenova@mnsu.edu>
2024-01-03 14:48:34 +0100

    Revert "trying to deploy"
    This reverts commit 88b240d91c756ffb1e25d7b92687445aadd9ecbb.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+
 import './App.scss';
-import { Routes, HashRouter, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 
+
+
 function App() {
   return (
     <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-        </Routes>
-      </HashRouter>
+    <Routes>
+    <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About/>} />
+          <Route path="contact" element={<Contact/>} />
+        </Route>
+    </Routes>
     </>
-  );
+  )
 }
 
 export default App;
